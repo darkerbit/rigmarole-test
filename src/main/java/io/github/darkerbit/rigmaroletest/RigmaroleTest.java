8b3bf05b26681bed3b569a2c7b3c860e030f4b89
DarkerBit <mail.darkerbit@gmail.com>
2020-10-11 18:02:23 +0300

    boots!

---
+++ src/main/java/io/github/darkerbit/rigmaroletest/RigmaroleTest.java
@@ -0,0 +1,10 @@
+package io.github.darkerbit.rigmaroletest;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RigmaroleTest implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello, world!");
+    }
+}
